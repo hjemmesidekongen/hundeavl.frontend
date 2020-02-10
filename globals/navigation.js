43125ae3228cfa47db4f4ca72f878752c827239b
Morten Nissen <mail@hjemmesidekongen.dk>
2020-02-10 15:34:35 +0100

    Extract navigation to a global file

---
+++ globals/navigation.js
@@ -0,0 +1,16 @@
+export const primaryLinks = [
+  { title: 'Startside', path: '/' },
+  { title: 'Priser', path: '/priser' },
+  { title: 'Spørgsmål & svar', path: '/spoergsmaal-og-svar' },
+  { title: 'Om os', path: '/om' },
+];
+
+export const secondaryLinks = [
+  { title: 'Vilkår & betingelser', path: '/betingelser' },
+  { title: 'Bliv partner', path: '/bliv-partner' },
+];
+
+export const userLinks = [
+  { title: 'Log ind', path: '/user/login' },
+  { title: 'Opret bruger', path: '/user/register' },
+];
