d72a565dc62099f0fcc1a547c8ea3cf887315275
Morten Nissen <mail@hjemmesidekongen.dk>
2020-02-10 15:28:26 +0100

    Add layout wrapper

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
-import Header from '../components/Header';
+import React from 'react';
 
-const MyApp = ({ Component, pageProps }) => <Component {...pageProps} />;
+import Layout from '../components/Layout';
+
+// eslint-disable-next-line react/prop-types
+const MyApp = ({ Component, pageProps }) => (
+  <Layout>
+    <Component {...pageProps} />
+  </Layout>
+);
 
 export default MyApp;
