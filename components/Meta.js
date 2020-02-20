5ad7f21d1c89f060a2c2a658a7b69413ae7b83bc
Morten Nissen <mail@hjemmesidekongen.dk>
2020-02-20 10:40:48 +0100

    Add favicons

--- components/Meta.js
+++ components/Meta.js
@@ -4,13 +4,16 @@ const Meta = () => (
   <Head>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta charSet="utf-8" />
-    <link rel="shortcut icon" href="/favicon.png" />
     <link rel="stylesheet" type="text/css" href="/styles/nprogress.css" />
     <link
       href="https://fonts.googleapis.com/css?family=Lato"
       rel="stylesheet"
     />
     <title>Hundeavl.dk</title>
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+    <link rel="manifest" href="/site.webmanifest" />
   </Head>
 );
 
