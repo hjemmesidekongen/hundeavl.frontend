8d2aeb8b82914bdf8c6e92340e9c19dd68c701d4
Morten Nissen <mail@hjemmesidekongen.dk>
2020-08-06 23:09:46 +0200

    Set up server config

---
+++ ecosystem.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  apps: [
+    {
+      name: 'hundeavl - produktion',
+      script: 'npm',
+      args: 'start',
+    },
+  ],
+  deploy: {
+    production: {
+      user: 'mn',
+      host: '64.225.99.176',
+      ref: 'origin/master',
+      repo: 'git@github.com:hjemmesidekongen/hundeavl.frontend.git',
+      path: '/var/www/hundeavl.dk/',
+      'pre-deploy-local': '',
+      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js',
+      'pre-setup': '',
+      env: {
+        NODE_ENV: 'production',
+        PORT: 4010,
+      },
+    },
+  },
+};
