70b5c0ef444a06804c762e1e5fb43ad43b5eaf9f
Morten Nissen <mail@hjemmesidekongen.dk>
2020-08-07 13:54:32 +0200

    Remove wesbos eslint

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,10 +7,24 @@ module.exports = {
     },
   ],
   deploy: {
-    production: {
+    staging: {
       user: 'mn',
       host: '64.225.99.176',
       ref: 'origin/develop',
+      repo: 'git@github.com:hjemmesidekongen/hundeavl.frontend.git',
+      path: '/var/www/hundeavl.dk/',
+      'pre-deploy-local': '',
+      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js',
+      'pre-setup': '',
+      env: {
+        NODE_ENV: 'development',
+        PORT: 4011,
+      },
+    },
+    production: {
+      user: 'mn',
+      host: '64.225.99.176',
+      ref: 'origin/master',
       repo: 'git@github.com:hjemmesidekongen/hundeavl.frontend.git',
       path: '/var/www/hundeavl.dk/',
       'pre-deploy-local': '',
